0b42ab5255643253244540d35778cfef835512db
claratorres1414 <claratorres12345679@gmail.com>
2025-06-20 15:49:38 -0300

    Subindo o UserDetails que acabou não subindo com as atualizações

---
+++ backend/src/main/java/com/PIEC/ImobLink/DTOs/UserDetails.java
@@ -0,0 +1,21 @@
+package com.PIEC.ImobLink.DTOs;
+
+import com.PIEC.ImobLink.Entitys.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDetails {
+    private String email;
+    private String name;
+    private String bio;
+    private String role;
+
+    public UserDetails(User user) {
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.bio = user.getBio();
+        this.role = user.getRole().toString();
+    }
+}
