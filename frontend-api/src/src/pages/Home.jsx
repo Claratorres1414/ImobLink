e90fb21c1c7f24b23c8018b27793b65b50285a1e
Mateus <mateusbarrosmagalhaes@gmail.com>
2025-06-23 17:51:11 -0300

    Delete frontend-api directory

--- frontend-api/src/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-100">
-      <h1 className="text-3xl font-bold">Bem-vindo à página principal do ImobLink!</h1>
-    </div>
-  );
-}
-
-export default Home;
